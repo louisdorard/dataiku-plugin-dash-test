eebd4f11e7dc653725b328b07ea417bc06a33acf
admin <louis.dorard@dataiku.com>
2022-03-18 11:22:16 +0100

    Edited files

--- webapps/show-dataset-name/backend.py
+++ webapps/show-dataset-name/backend.py
@@ -1,11 +1,5 @@
 from dataiku.customwebapp import *
-
-
 from dash import html
-import dataiku
 
 ds = get_webapp_config()["input_dataset"]
-
-app.layout = html.Div([
-    html.H4('Edit ' + ds.full_name)
-])
+app.layout = html.Div(ds.full_name)
