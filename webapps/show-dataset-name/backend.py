f148e062afcb9da05e23cd0478f8d8198d74c0dc
admin <louis.dorard@dataiku.com>
2022-03-18 11:17:59 +0100

    Edited file 'webapps/show-dataset-name/backend.py'

--- webapps/show-dataset-name/backend.py
+++ webapps/show-dataset-name/backend.py
@@ -1,14 +1,10 @@
 from dataiku.customwebapp import *
 
-# Access the parameters that end-users filled in using webapp config
-# For example, for a parameter called "input_dataset"
-# input_dataset = get_webapp_config()["input_dataset"]
 
 from dash import html
 import dataiku
 
-ds = dataiku.Dataset("iris")
-df = ds.get_dataframe()
+ds = get_webapp_config()["input_dataset"]
 
 app.layout = html.Div([
     html.H4('Edit ' + ds.full_name)
